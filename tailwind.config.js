4aefd7890799a8a52445a0327a6592cc2c8270aa
Gabriel Fernandes <contato.gfernandesribeiro@gmail.com>
2022-11-09 23:29:08 -0300

    Creates navbar component

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        "app-red": "#e4000f",
+      },
+    },
+  },
+  plugins: [],
+};
