cfa9ae2f26b8dd8bd1869095fa1114256c435b7c
Gabriel Fernandes <contato.gfernandesribeiro@gmail.com>
2022-11-15 17:07:25 -0300

    Adds font awesome, improves artist mock page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ArtistComponent } from './pages/artist/artist.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent, HomeComponent, ArtistComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/artist/artist.component.ts
+++ src/app/pages/artist/artist.component.ts
@@ -1,17 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  faSpotify,
+  faYoutube,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-artist',
   templateUrl: './artist.component.html',
 })
 export class ArtistComponent implements OnInit {
-  name = 'takayan'
-  image = 'https://akamai.sscdn.co/letras/338x298/fotos/f/5/0/4/f50463517748ddaebd314419cf365203.jpg'
-  country = 'JAPAN'
+  name = 'takayan';
+  image =
+    'https://akamai.sscdn.co/letras/338x298/fotos/f/5/0/4/f50463517748ddaebd314419cf365203.jpg';
+  country = 'JAPAN';
+  spotifyUrl = 'https://open.spotify.com/artist/79JgYJHiBUZcvYqOC5Zufn'
+  youtubeUrl = 'https://www.youtube.com/channel/UCHGGjDM6Z7nq5qBIPzn1D7Q'
+  twitterUrl = 'https://twitter.com/takayan_gorizal'
+  spotifyIcon = faSpotify;
+  youtubeIcon = faYoutube;
+  twitterIcon = faTwitter;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  getImageUrl(): string {
+    return `url(${this.image})`;
   }
-
 }
